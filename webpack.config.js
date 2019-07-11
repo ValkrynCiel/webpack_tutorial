2bf2e03ce716a5d3b81271a77b7f58c727147587
Ethan Shen <ethanshen.wu@gmail.com>
2019-07-11 15:19:12 -0700

    webpack.config.js: added absolute path to output, fixed typos

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@
  * output - specify the name of the file that will be generated and path to the directory where the file will be generated
  * webpack will automatically create the folder if it doesn't exist
  */
+
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    filname: 'bundle.js',
-    path: './dist',
-    mode: 'none'
-  }
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  mode: 'none'
 }
